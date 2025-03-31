5dd3121e45e5d82c3a1de9340496d5262caaf49e
CoolPerson1237 <144965803+CoolPerson1237@users.noreply.github.com>
2025-03-31 11:16:07 -0400

    Update homeScript.js

--- homeScript.js
+++ homeScript.js
@@ -1,4 +1,5 @@
 document.body.style.backgroundColor = "lightblue";
   function toDrive(){
-  document.location.href = https://drive.google.com/drive/folders/1UeKfRxkIYm4aGGzOGOxlla_uFB826MoB;
+  window.location = https://drive.google.com/drive/folders/1UeKfRxkIYm4aGGzOGOxlla_uFB826MoB;
   }
+  
