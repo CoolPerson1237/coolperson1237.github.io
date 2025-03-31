4b704ea2f861723906d3e4d8da2a95a2f1a2d335
CoolPerson1237 <144965803+CoolPerson1237@users.noreply.github.com>
2025-03-31 10:58:00 -0400

    Update and rename script.js to homeScript.js

--- script.js
+++
@@ -1,7 +0,0 @@
-  document.getElementById('content').textContent = 'This text is added by JavaScript.';
-  function testFun(){
-    document.getElementById('changes').textContent = 'change';
-  }
-  function switchPage(){
-  window.location.href = https://drive.google.com/drive/folders/1UeKfRxkIYm4aGGzOGOxlla_uFB826MoB;
-  }
