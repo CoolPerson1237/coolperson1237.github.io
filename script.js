94b393d515193222e12315b0b267b1c85ba79600
CoolPerson1237 <144965803+CoolPerson1237@users.noreply.github.com>
2025-03-31 12:41:16 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ function please(){
   document.getElementById('content').textContent = 'This was created using Javascript.';
 }
 function changeBack(){
-  document.body.style.backgroundColor = "#F03212";
+  document.body.style.backgroundColor = "#12d0f0";
 }
