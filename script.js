4a83bc4d1416f30fc4de4d82e4e53294efb4fa3d
CoolPerson1237 <144965803+CoolPerson1237@users.noreply.github.com>
2025-03-31 12:22:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('eddie').textContent = 'help.';
 console.log('hello world');
 function please(){
-  document.getElementById('eddie').textContent = 'This text is added by JavaScript.';
+  document.getElementById('content').textContent = 'This text is added by JavaScript.';
 }
 function changeColor(){
   document.body.style.backgroundColor = "pink";
