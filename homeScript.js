6e74859a65b987cac3c795787493898d92e08545
CoolPerson1237 <144965803+CoolPerson1237@users.noreply.github.com>
2025-03-31 11:34:02 -0400

    Update homeScript.js

--- homeScript.js
+++ homeScript.js
@@ -1,5 +1,7 @@
-document.getElementById('content').textContent = 'This text is added by JavaScript.';
 
+function please(){
+  document.getElementById('content').textContent = 'This text is added by JavaScript.';
+}
 function changeColor(){
   document.body.style.backgroundColor = "pink";
 }
