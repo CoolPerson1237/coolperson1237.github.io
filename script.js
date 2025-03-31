275a883176cb7250276f481d2288bf1b02e01a6a
CoolPerson1237 <144965803+CoolPerson1237@users.noreply.github.com>
2025-03-31 12:25:46 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 document.getElementById('eddie').textContent = 'help.';
-console.log('hello world');
+
 function please(){
-  document.getElementById('content').textContent = 'kys';
+  document.getElementById('content').textContent = 'This was created using Javascript.';
 }
-function changeColor(){
-  document.body.style.backgroundColor = "pink";
-}
-  function toDrive(){
-  window.location = https://drive.google.com/drive/folders/1UeKfRxkIYm4aGGzOGOxlla_uFB826MoB;
-  }
-  
