23516a91e5465c4a7f1882ad0c8e7e6dc7260da6
CoolPerson1237 <144965803+CoolPerson1237@users.noreply.github.com>
2025-03-31 11:52:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 document.getElementById('eddie').textContent = 'help.';
+console.log('hello world');
 function please(){
   document.getElementById('content').textContent = 'This text is added by JavaScript.';
 }
